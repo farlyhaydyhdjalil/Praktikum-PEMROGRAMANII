c8bb73ee5a3581d441b5f69ac5937c237b8927d6
farlyhaydydjalil <farlyhaydy123@gmail.com>
2023-10-27 01:24:09 +0800

    commit

---
+++ Modul 3/src/soal1/Main.java
@@ -0,0 +1,14 @@
+package soal1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Dadu dd = new Dadu();
+        int masukkan = input.nextInt();
+
+        dd.setInput(masukkan);
+        dd.acakNilai();
+    }
+}
